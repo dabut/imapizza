5688c7e25241799bcb5e8be9e74e4e3b09184e26
Dylan Butler <dylanholden@live.com.au>
2016-03-07 20:01:30 +1100

    Add image and click count

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
+	include 'assets/config.php';
 
+	$query = mysqli_query($db, "SELECT * FROM clicks");
+	while ($row = mysqli_fetch_array($query)) {
+		$clicks = $row['clicks'];
+	}
 ?>
+<!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="assets/style.css" />
+		<script type="text/javacsript" src="assets/jquery.js"></script>
 		<title>I'm A Pizza!</title>
 	</head>
 	<body>
+		<img src="assets/pizza.jpg" />
+		<p id="clicks"><?=$clicks?></p><p> Clicks!</p>
+		<script>
+			
+		</script>
 	</body>
 </html>
